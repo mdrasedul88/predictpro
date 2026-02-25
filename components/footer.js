48a8d87dd7c829ec07abb487c94ad542347f9867
mdrasedul88 <rasedulinfinix@gmail.com>
2026-02-25 16:58:40 +0600

    Delete components directory

--- components/footer.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('footer-placeholder').innerHTML = `
-<footer>
-    <div class="container">
-        <p>&copy; 2026 Sports Look Prediction. All Rights Reserved.</p>
-        <p><small>Privacy Policy | Terms of Service</small></p>
-    </div>
-</footer>
-`;
-
