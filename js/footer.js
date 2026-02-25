a1b2aec3adfeb8d306769571fb2428f88e1080a4
mdrasedul88 <rasedulinfinix@gmail.com>
2026-02-25 19:51:01 +0600

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,14 +1,16 @@
-const footerHTML = `
+const footerTemplate = `
 <footer class="fix-nav">
     <nav class="nav-bar">
-        <a href="#" class="nav-item active"><i class="fa-solid fa-house"></i><span>Home</span></a>
-        <a href="#" class="nav-item"><i class="fa-solid fa-tower-broadcast"></i><span>Live</span></a>
-        <a href="#" class="nav-item"><i class="fa-solid fa-gift"></i><span>Free</span></a>
-        <a href="#" class="nav-item"><i class="fa-solid fa-crown"></i><span>Vip</span></a>
-        <a href="#" class="nav-item"><i class="fa-solid fa-newspaper"></i><span>Blog</span></a>
+        <a href="index.html" class="nav-item"><i class="fa-solid fa-house"></i><span>Home</span></a>
+        
+        <a href="live.html" class="nav-item"><i class="fa-solid fa-tower-broadcast"></i><span>Live</span></a>
+        <a href="free.html" class="nav-item"><i class="fa-solid fa-gift"></i><span>Free</span></a>
+        <a href="vip.html" class="nav-item"><i class="fa-solid fa-crown"></i><span>Vip</span></a>
+        <a href="blog.html" class="nav-item"><i class="fa-solid fa-newspaper"></i><span>Blog</span></a>
+        
         <a href="profile.html" class="nav-item"><i class="fa-solid fa-user"></i><span>Profile</span></a>
     </nav>
 </footer>
 `;
 
-document.getElementById('footer-root').innerHTML = footerHTML;
+document.getElementById('footer-root').innerHTML = footerTemplate;
