e9711ce6dc0c9a15d21ecf3fae51433953710ec8
mdrasedul88 <rasedulinfinix@gmail.com>
2026-02-25 15:18:25 +0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,31 @@
+async function loadPage(page){
 
+    try{
+
+        const response = await fetch(page+".html");
+        const html = await response.text();
+
+        document.getElementById("mainContent").innerHTML = html;
+
+        window.history.pushState({}, "", page);
+
+        window.scrollTo(0,0);
+
+    }catch(err){
+        console.log(err);
+    }
+}
+
+/* Navigation Click Handler */
+
+document.querySelectorAll(".nav-link").forEach(link=>{
+    link.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        const page = this.getAttribute("data-page");
+
+        loadPage(page);
+
+    });
+});
