122c20bc82ba55c85f471787ac66b33681425c0c
mdrasedul88 <rasedulinfinix@gmail.com>
2026-02-25 19:12:52 +0600

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,12 +1,15 @@
-<header class="fixed top-0 left-0 right-0 bg-[#0f172a] border-b border-gray-800 px-4 py-3 flex justify-between items-center z-50">
-    <div class="relative cursor-pointer">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-        </svg>
-        <span class="absolute top-0 right-0 block h-2 w-2 rounded-full bg-red-600 ring-2 ring-[#0f172a]"></span>
-    </div>
-
-    <div class="text-xl font-black tracking-tighter text-white uppercase italic">
-        SPORTS<span class="text-yellow-500">PRO</span>
+const headerHTML = `
+<header class="main-header">
+    <div class="header-container">
+        <div class="notif-icon">
+            <i class="fa-regular fa-bell"></i>
+            <span class="badge"></span>
+        </div>
+        <div class="site-logo">
+            <h1>SPORTS<span>PRO</span></h1>
+        </div>
     </div>
 </header>
+`;
+
+document.getElementById('header-root').innerHTML = headerHTML;
