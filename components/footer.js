8afb72c78fddd3f1d5adb397f3cf83ca5b843452
mdrasedul88 <rasedulinfinix@gmail.com>
2026-02-25 16:07:20 +0600

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,1 +1,9 @@
+document.getElementById('footer-placeholder').innerHTML = `
+<footer>
+    <div class="container">
+        <p>&copy; 2026 Sports Look Prediction. All Rights Reserved.</p>
+        <p><small>Privacy Policy | Terms of Service</small></p>
+    </div>
+</footer>
+`;
 
