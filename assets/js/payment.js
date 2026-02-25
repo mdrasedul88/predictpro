565e32f7091c6a40156e1f9fea46c88a5e8d4619
mdrasedul88 <rasedulinfinix@gmail.com>
2026-02-25 15:29:25 +0600

    Update payment.js

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -1,1 +1,31 @@
+async function unlockPrediction(predictionId){
 
+    const user = await supabase.auth.getUser();
+
+    if(!user.data.user){
+        alert("Please login first");
+        return;
+    }
+
+    showRewardAd();
+
+    setTimeout(async ()=>{
+
+        const expiry = new Date();
+        expiry.setHours(expiry.getHours()+24);
+
+        await supabase.from("unlock_logs").insert([
+            {
+                user_id:user.data.user.id,
+                prediction_id:predictionId,
+                unlock_time:new Date(),
+                expire_time:expiry,
+                status:"active"
+            }
+        ]);
+
+        loadPage("free");
+
+    },30000); // Ad watch simulation
+
+}
