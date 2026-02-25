cd5cbe06eca67af82f83bdfc711bbd3362612ba4
mdrasedul88 <rasedulinfinix@gmail.com>
2026-02-25 20:13:58 +0600

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -7,7 +7,6 @@ const footerTemplate = `
         <a href="free.html" class="nav-item"><i class="fa-solid fa-gift"></i><span>Free</span></a>
         <a href="vip.html" class="nav-item"><i class="fa-solid fa-crown"></i><span>Vip</span></a>
         <a href="blog.html" class="nav-item"><i class="fa-solid fa-newspaper"></i><span>Blog</span></a>
-        
         <a href="profile.html" class="nav-item"><i class="fa-solid fa-user"></i><span>Profile</span></a>
     </nav>
 </footer>
