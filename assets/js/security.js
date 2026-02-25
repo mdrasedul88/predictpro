9499bbea6806b5685ed933cd6b5ba95c2d3bbb06
mdrasedul88 <rasedulinfinix@gmail.com>
2026-02-25 15:07:44 +0600

    Update security.js

--- assets/js/security.js
+++ assets/js/security.js
@@ -1,1 +1,15 @@
+async function generateSecurityToken(){
 
+    const user = await supabase.auth.getUser();
+
+    if(!user.data.user) return;
+
+    const token = crypto.randomUUID();
+
+    await supabase
+    .from("users")
+    .update({
+        security_token: token
+    })
+    .eq("id", user.data.user.id);
+}
